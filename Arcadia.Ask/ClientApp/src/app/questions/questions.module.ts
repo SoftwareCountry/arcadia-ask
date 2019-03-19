6234757c3eaff0b9e1c51c622c120cdfb48ce89f
Alexander Shevnin <alexander.shevnin@arcadia.spb.ru>
2019-03-19 14:51:43 +0300

    some styling applied

--- Arcadia.Ask/ClientApp/src/app/questions/questions.module.ts
+++ Arcadia.Ask/ClientApp/src/app/questions/questions.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import { MatInputModule, MatFormFieldModule, MatButtonModule } from '@angular/material';
 import { QuestionsRoutingModule } from './questions-routing.module';
 import { QuestionsListComponent } from './questions-list/questions-list.component';
 import { AddQuestionFormComponent } from './add-question-form/add-question-form.component';
@@ -13,7 +13,10 @@ import { QuestionCardComponent } from './question-card/question-card.component';
   imports: [
     CommonModule,
     QuestionsRoutingModule,
-    FormsModule
+    FormsModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatInputModule
   ]
 })
 export class QuestionsModule { }
