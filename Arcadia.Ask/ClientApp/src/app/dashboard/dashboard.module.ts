cbffa09664caf1caaf0ad05286a0553bee98a08f
Alexander Shevnin <alexander.shevnin@arcadia.spb.ru>
2019-03-19 15:22:01 +0300

    fixed modules

--- Arcadia.Ask/ClientApp/src/app/dashboard/dashboard.module.ts
+++ Arcadia.Ask/ClientApp/src/app/dashboard/dashboard.module.ts
@@ -4,10 +4,12 @@ import { CommonModule } from '@angular/common';
 import { DashboardRoutingModule } from './dashboard-routing.module';
 import { MainComponent } from './main/main.component';
 import { QuestionsModule } from '../questions/questions.module';
+import { MatGridListModule } from '@angular/material';
 
 @NgModule({
   declarations: [MainComponent],
   imports: [
+    MatGridListModule,
     CommonModule,
     DashboardRoutingModule,
     QuestionsModule
--- Arcadia.Ask/ClientApp/src/app/questions/questions.module.ts
+++ Arcadia.Ask/ClientApp/src/app/questions/questions.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { MatInputModule, MatFormFieldModule, MatButtonModule } from '@angular/material';
+import { MatInputModule, MatFormFieldModule, MatButtonModule, MatListModule, MatIconModule } from '@angular/material';
 import { QuestionsRoutingModule } from './questions-routing.module';
 import { QuestionsListComponent } from './questions-list/questions-list.component';
 import { AddQuestionFormComponent } from './add-question-form/add-question-form.component';
@@ -14,6 +14,8 @@ import { QuestionCardComponent } from './question-card/question-card.component';
     CommonModule,
     QuestionsRoutingModule,
     FormsModule,
+    MatIconModule,
+    MatListModule,
     MatButtonModule,
     MatFormFieldModule,
     MatInputModule
