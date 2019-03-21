d2c1d3fdd5cada55a7ddf69f07f0db16d0f31432
dmitriy.khlopkov <dmitriy.khlopkov@arcadia.spb.ru>
2019-03-21 17:33:11 +0300

    added models describing entities

---
+++ Arcadia.Ask/Models/Entities/VoteEntity.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Arcadia.Ask.Models.Entities
+{
+    public class VoteEntity
+    {
+        public Guid QuestionId { get; set; }
+
+        public Guid UserId { get; set; }
+
+        public virtual QuestionEntity Question { get; set; }
+    }
+}
