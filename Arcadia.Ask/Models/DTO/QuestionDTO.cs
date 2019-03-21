6eef9cc00614475979ca67a37ff674cf3bc7acf3
dmitriy.khlopkov <dmitriy.khlopkov@arcadia.spb.ru>
2019-03-21 17:22:16 +0300

    added DTO for question

---
+++ Arcadia.Ask/Models/DTO/QuestionDTO.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Arcadia.Ask.Models.DTO
+{
+    public class QuestionDTO
+    {
+        public Guid QuestionId { get; set; }
+
+        public string Text { get; set; }
+
+        public string Author { get; set; }
+
+        public DateTimeOffset PostedAt { get; set; }
+
+        public bool IsApproved { get; set; }
+
+        public IEnumerable<Guid> Votes { get; set; }
+    }
+}
