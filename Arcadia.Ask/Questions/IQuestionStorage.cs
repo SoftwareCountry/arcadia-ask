963443a2814e94dd073f14c36bc50c1ca3fb460e
dmitriy.khlopkov <dmitriy.khlopkov@arcadia.spb.ru>
2019-03-21 18:51:07 +0300

    changed returning values of Storage layer to Dtos

--- Arcadia.Ask/Questions/IQuestionStorage.cs
+++ Arcadia.Ask/Questions/IQuestionStorage.cs
@@ -1,20 +1,22 @@
 ﻿namespace Arcadia.Ask.Questions
 {
+    using Arcadia.Ask.Models.DTO;
     using System;
+    using System.Collections.Generic;
     using System.Threading.Tasks;
 
     public interface IQuestionStorage
     {
-        Task<Question[]> GetQuestions();
+        Task<IEnumerable<QuestionDTO>> GetQuestions();
 
-        Task<Question> GetQuestion(Guid questionId);
-
-        Task<Question> UpsertQuestion(Question question);
+        Task<QuestionDTO> GetQuestion(Guid questionId);
+        
+        Task<QuestionDTO> UpsertQuestion(QuestionDTO question);
 
         Task DeleteQuestion(Guid questionId);
 
-        Task<Question> ApproveQuestion(Guid questionId);
+        Task<QuestionDTO> ApproveQuestion(Guid questionId);
 
-        Task<Question> ChangeVotes(Guid questionId, int diff);
+        Task<QuestionDTO> ChangeVotes(Guid questionId, int diff);
     }
 }
