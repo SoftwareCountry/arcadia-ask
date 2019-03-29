032e007948bbfdb531ba52d5bb1cf44aa5a91fe2
dmitriy.khlopkov <dmitriy.khlopkov@arcadia.spb.ru>
2019-03-29 13:39:04 +0300

    created models describing database entities

---
+++ Arcadia.Ask/Models/Entities/QuestionEntity.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Arcadia.Ask.Models.Entities
+{
+    public class QuestionEntity
+    {
+        public Guid QuestionId { get; set; }
+
+        public string Text { get; set; }
+
+        public string Author { get; set; }
+
+        public DateTimeOffset PostedAt { get; set; }
+
+        public bool IsApproved { get; set; }
+
+        public virtual ICollection<VoteEntity> Votes { get; set; }
+    }
+}
