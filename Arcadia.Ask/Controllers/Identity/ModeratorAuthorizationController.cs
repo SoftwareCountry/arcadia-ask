44f190d6a11726ef21b65b736fae4b5525e17869
dmitriy.khlopkov <dmitriy.khlopkov@arcadia.spb.ru>
2019-04-04 16:05:32 +0300

    Adde controller for authorization as moderator.

---
+++ Arcadia.Ask/Controllers/Identity/ModeratorAuthorizationController.cs
@@ -0,0 +1,40 @@
+﻿namespace Arcadia.Ask.Controllers.Identity
+{
+    using System;
+    using System.Security.Claims;
+    using System.Threading.Tasks;
+
+    using Microsoft.AspNetCore.Authentication;
+    using Microsoft.AspNetCore.Mvc;
+
+    [Route("auth/moderator")]
+    public class ModeratorAuthorizationController : Controller
+    {
+        [Route("sign-in")]
+        [HttpPost]
+        public async Task<IActionResult> AuthorizeModerator()
+        {
+            const string roleName = "Moderator";
+
+            if (this.User.IsInRole(roleName))
+            {
+                return this.Ok();
+            }
+
+            var guid = this.User.Identity.IsAuthenticated ? 
+                Guid.Parse(this.User.Identity.Name) : Guid.NewGuid();
+
+            var claims = new[]
+            {
+                new Claim(ClaimTypes.Name, guid.ToString()),
+                new Claim(ClaimTypes.Role, roleName), 
+            };
+            var identity = new ClaimsIdentity(claims, roleName);
+            var principal = new ClaimsPrincipal(identity);
+
+            await this.HttpContext.SignInAsync(principal);
+
+            return this.Ok();
+        }
+    }
+}
