67563a7cccf1df9f2e7e2fcd592371dd0ab2a884
dmitriy.khlopkov <dmitriy.khlopkov@arcadia.spb.ru>
2019-04-08 13:51:17 +0300

    Added service fetching permissions

---
+++ Arcadia.Ask/ClientApp/src/app/identity/permissions.service.ts
@@ -0,0 +1,13 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Permissions } from './permissions';
+
+@Injectable()
+export class PermissionService {
+  constructor(private readonly http: HttpClient) { }
+
+  public getPermissions(): Observable<Permissions>{
+    return this.http.get<Permissions>('/api/permissions');
+  }
+}
