c45123bad1779769a96336401609b5a38d0588be
dmitriy.khlopkov <dmitriy.khlopkov@arcadia.spb.ru>
2019-04-08 12:51:04 +0300

    Added classes describing permissions of role and creating them by role

---
+++ Arcadia.Ask/Auth/Permissions/PermissionsByRoleCreator.cs
@@ -0,0 +1,25 @@
+﻿namespace Arcadia.Ask.Auth.Permissions
+{
+    using Arcadia.Ask.Auth.Roles;
+
+    public class PermissionsByRoleCreator
+    {
+        public IPermissions Permissions { get; }
+
+        public PermissionsByRoleCreator(string role)
+        {
+            if (role == Roles.Moderator)
+            {
+                this.Permissions = new ModeratorPermissions();
+            }
+            else if (role == Roles.User)
+            {
+                this.Permissions = new UserPermissions();
+            }
+            else
+            {
+                throw new UnknownRoleException(role);
+            }
+        }
+    }
+}
---
+++ Arcadia.Ask/Auth/Roles/UnknownRoleException.cs
@@ -0,0 +1,9 @@
+﻿namespace Arcadia.Ask.Auth.Roles
+{
+    using System;
+
+    public class UnknownRoleException : Exception
+    {
+        public UnknownRoleException(string role) : base($"Role {role} is unknown") { }
+    }
+}
