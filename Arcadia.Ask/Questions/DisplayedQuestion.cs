ab269eb655a9db3b4c0e1bef6e89cb40f6483db7
dmitriy.khlopkov <dmitriy.khlopkov@arcadia.spb.ru>
2019-04-10 16:06:45 +0300

    Removed redundant read/write locking of displayed question id

--- Arcadia.Ask/Questions/DisplayedQuestion.cs
+++ Arcadia.Ask/Questions/DisplayedQuestion.cs
@@ -1,45 +1,9 @@
 ﻿namespace Arcadia.Ask.Questions
 {
     using System;
-    using System.Threading;
 
     public class DisplayedQuestion : IDisplayedQuestion
     {
-        private Guid currentDisplayedQuestionId;
-        private readonly ReaderWriterLock rwLock = new ReaderWriterLock();
-
-        public Guid CurrentDisplayedQuestionId
-        {
-            get
-            {
-                this.rwLock.AcquireReaderLock(-1);
-
-                Guid result;
-
-                try
-                {
-                    result = this.currentDisplayedQuestionId;
-                }
-                finally
-                {
-                    this.rwLock.ReleaseReaderLock();
-                }
-
-                return result;
-            }
-            set
-            {
-                this.rwLock.AcquireWriterLock(-1);
-
-                try
-                {
-                    this.currentDisplayedQuestionId = value;
-                }
-                finally
-                {
-                    this.rwLock.ReleaseWriterLock();
-                }
-            }
-        }
+        public Guid CurrentDisplayedQuestionId { get; set; }
     }
 }
