17e892c94446d697914d77707c735824447d6903
dmitriy.khlopkov <dmitriy.khlopkov@arcadia.spb.ru>
2019-04-10 13:02:55 +0300

    Added class for storing current displayed questions

---
+++ Arcadia.Ask/Questions/DisplayedQuestion.cs
@@ -0,0 +1,45 @@
+﻿namespace Arcadia.Ask.Questions
+{
+    using System;
+    using System.Threading;
+
+    public class DisplayedQuestion : IDisplayedQuestion
+    {
+        private Guid currentDisplayedQuestionId;
+        private readonly ReaderWriterLock rwLock = new ReaderWriterLock();
+
+        public Guid CurrentDisplayedQuestionId
+        {
+            get
+            {
+                this.rwLock.AcquireReaderLock(-1);
+
+                Guid result;
+
+                try
+                {
+                    result = this.currentDisplayedQuestionId;
+                }
+                finally
+                {
+                    this.rwLock.ReleaseReaderLock();
+                }
+
+                return result;
+            }
+            set
+            {
+                this.rwLock.AcquireWriterLock(-1);
+
+                try
+                {
+                    this.currentDisplayedQuestionId = value;
+                }
+                finally
+                {
+                    this.rwLock.ReleaseWriterLock();
+                }
+            }
+        }
+    }
+}
