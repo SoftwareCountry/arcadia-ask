6b669a5cd25f494af1279afdf83b470241a7b58b
dmitriy.khlopkov <dmitriy.khlopkov@arcadia.spb.ru>
2019-04-10 12:22:06 +0300

    Added showing popup on created question

--- Arcadia.Ask/ClientApp/src/app/questions/add-question-form/add-question-form.component.ts
+++ Arcadia.Ask/ClientApp/src/app/questions/add-question-form/add-question-form.component.ts
@@ -1,5 +1,7 @@
 import { Component, ChangeDetectionStrategy } from '@angular/core';
 import { QuestionsStore } from '../questions-store.service';
+import { MatDialog } from '@angular/material';
+import { QuestionCreatedPopupComponent } from './question-created-popup/question-created-popup.component';
 
 @Component({
   selector: 'app-add-question-form',
@@ -12,9 +14,23 @@ export class AddQuestionFormComponent {
     text: ''
   };
 
-  constructor(private readonly questionsStore: QuestionsStore) { }
+  constructor(
+    private readonly questionsStore: QuestionsStore,
+    private readonly dialog: MatDialog,
+  ) { }
 
   public onSubmit() {
-    this.questionsStore.createQuestion(this.model.text).catch(x => console.error(x));
+    this.questionsStore.createQuestion(this.model.text)
+      .then(() => this.openDialog())
+      .then(() => this.model.text = '')
+      .catch(x => console.error(x));
+  }
+
+  private openDialog(): void {
+    this.dialog.open(
+      QuestionCreatedPopupComponent, {
+        data: {questionText: this.model.text}
+      }
+    );
   }
 }
