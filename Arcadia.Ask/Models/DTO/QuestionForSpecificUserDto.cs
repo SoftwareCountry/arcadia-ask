66f2fcc0986ce3a32deabf30d36962ff2d0fbb19
dmitriy.khlopkov <dmitriy.khlopkov@arcadia.spb.ru>
2019-04-11 16:45:10 +0300

    Revert "Converting EntityToDtoForSpecificUser moved to QuestionForSpecificUserDto constructor"
    This reverts commit eba970832214c03f462fc62790c362519ec1215e.

--- Arcadia.Ask/Models/DTO/QuestionForSpecificUserDto.cs
+++ Arcadia.Ask/Models/DTO/QuestionForSpecificUserDto.cs
@@ -1,25 +1,9 @@
 ﻿namespace Arcadia.Ask.Models.DTO
 {
-    using System;
-    using System.Linq;
-
-    using Arcadia.Ask.Models.Entities;
-
     public class QuestionForSpecificUserDto
     {
         public QuestionDto Metadata { get; set; }
 
         public bool DidVote { get; set; }
-
-        public QuestionForSpecificUserDto(QuestionEntity entity, Guid userId)
-        {
-            if (entity == null)
-            {
-                throw new ArgumentNullException(nameof(entity));
-            }
-
-            this.Metadata = new QuestionDto(entity);
-            this.DidVote = entity.Votes?.Any(v => v.UserId == userId) ?? false;
-        }
     }
 }
