83c36b3697f830fd112cc3881fc39a6ea5aab701
dmitriy.khlopkov <dmitriy.khlopkov@arcadia.spb.ru>
2019-04-11 17:09:10 +0300

    Fixed reseting form input fields

--- Arcadia.Ask/ClientApp/src/app/questions/add-question-form/add-question-form.component.ts
+++ Arcadia.Ask/ClientApp/src/app/questions/add-question-form/add-question-form.component.ts
@@ -2,6 +2,7 @@ import { Component, ChangeDetectionStrategy } from '@angular/core';
 import { QuestionsStore } from '../questions-store.service';
 import { MatDialog } from '@angular/material';
 import { QuestionCreatedPopupComponent } from './question-created-popup/question-created-popup.component';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-add-question-form',
@@ -19,10 +20,10 @@ export class AddQuestionFormComponent {
     private readonly dialog: MatDialog,
   ) { }
 
-  public onSubmit() {
+  public onSubmit(form: NgForm) {
     this.questionsStore.createQuestion(this.model.text)
       .then(() => this.notifyAboutQuestionCreated())
-      .then(() => this.model.text = '')
+      .then(() => form.resetForm())
       .catch(x => console.error(x));
   }
 
