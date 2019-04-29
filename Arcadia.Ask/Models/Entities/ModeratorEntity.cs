69911b033f747e0f3a33b80db0011b7add1aa212
dmitriy.khlopkov <dmitriy.khlopkov@arcadia.spb.ru>
2019-04-29 12:08:07 +0300

    Created entity for moderator

---
+++ Arcadia.Ask/Models/Entities/ModeratorEntity.cs
@@ -0,0 +1,11 @@
+﻿namespace Arcadia.Ask.Models.Entities
+{
+    using System.ComponentModel.DataAnnotations;
+
+    public class ModeratorEntity
+    {
+        [Key]
+        public string Login { get; set; }
+        public string Hash { get; set; }
+    }
+}
