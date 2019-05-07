ccfc69c3c52421a00ef675166fcf0c5a868093d6
dmitriy.khlopkov <dmitriy.khlopkov@arcadia.spb.ru>
2019-05-07 13:15:25 +0300

    Add UserRoles to databased context
    Added UserRoles field with DbSet of UserRoleEntities to DatabaseContext

--- Arcadia.Ask/Storage/DatabaseContext.cs
+++ Arcadia.Ask/Storage/DatabaseContext.cs
@@ -49,5 +49,7 @@
         public DbSet<UserEntity> Users { get; set; }
 
         public DbSet<RoleEntity> Roles { get; set; }
+
+        public DbSet<UserRoleEntity> UserRoles { get; set; }
     }
 }
