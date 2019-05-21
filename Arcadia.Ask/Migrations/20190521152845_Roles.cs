e336b2b6853403f4e934057670ec28bc92de642d
Alexander Shevnin <alexander.shevnin@arcadia.spb.ru>
2019-05-21 18:54:51 +0300

    fixing migrations manually

--- Arcadia.Ask/Migrations/20190521152845_Roles.cs
+++ Arcadia.Ask/Migrations/20190521152845_Roles.cs
@@ -7,57 +7,6 @@ namespace Arcadia.Ask.Migrations
     {
         protected override void Up(MigrationBuilder migrationBuilder)
         {
-            migrationBuilder.DropTable(
-                name: "Moderators");
-
-            migrationBuilder.CreateTable(
-                name: "Roles",
-                columns: table => new
-                {
-                    RoleId = table.Column<Guid>(nullable: false),
-                    Name = table.Column<string>(nullable: true)
-                },
-                constraints: table =>
-                {
-                    table.PrimaryKey("PK_Roles", x => x.RoleId);
-                });
-
-            migrationBuilder.CreateTable(
-                name: "Users",
-                columns: table => new
-                {
-                    Login = table.Column<string>(nullable: false),
-                    Hash = table.Column<string>(nullable: true)
-                },
-                constraints: table =>
-                {
-                    table.PrimaryKey("PK_Users", x => x.Login);
-                });
-
-            migrationBuilder.CreateTable(
-                name: "UserRoles",
-                columns: table => new
-                {
-                    UserLogin = table.Column<string>(nullable: false),
-                    RoleId = table.Column<Guid>(nullable: false)
-                },
-                constraints: table =>
-                {
-                    table.PrimaryKey("PK_UserRoles", x => new { x.UserLogin, x.RoleId });
-                    table.ForeignKey(
-                        name: "FK_UserRoles_Roles_RoleId",
-                        column: x => x.RoleId,
-                        principalTable: "Roles",
-                        principalColumn: "RoleId",
-                        onDelete: ReferentialAction.Cascade);
-                    table.ForeignKey(
-                        name: "FK_UserRoles_Users_UserLogin",
-                        column: x => x.UserLogin,
-                        principalTable: "Users",
-                        principalColumn: "Login",
-                        onDelete: ReferentialAction.Cascade);
-                });
-
             migrationBuilder.InsertData(
                 table: "Roles",
                 columns: new[] { "RoleId", "Name" },
@@ -67,11 +16,6 @@ namespace Arcadia.Ask.Migrations
                 table: "Roles",
                 columns: new[] { "RoleId", "Name" },
                 values: new object[] { new Guid("835f137e-4b44-4e7b-8563-849eb151fd74"), "Moderator" });
-
-            migrationBuilder.CreateIndex(
-                name: "IX_UserRoles_RoleId",
-                table: "UserRoles",
-                column: "RoleId");
         }
 
         protected override void Down(MigrationBuilder migrationBuilder)
