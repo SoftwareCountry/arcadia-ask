958fef130cc2a822d0dc6e7ce80936c7c9276673
dmitriy.khlopkov <dmitriy.khlopkov@arcadia.spb.ru>
2019-05-23 14:14:06 +0300

    Hide sign in link for users, who already signed in as moderator

--- Arcadia.Ask/ClientApp/src/app/nav-menu/nav-menu.component.ts
+++ Arcadia.Ask/ClientApp/src/app/nav-menu/nav-menu.component.ts
@@ -1,4 +1,7 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { PermissionService } from '../identity/permissions.service';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-nav-menu',
@@ -7,4 +10,11 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class NavMenuComponent {
+  public isModerator$: Observable<boolean>;
+
+  constructor(permissionsService: PermissionService) { 
+    this.isModerator$ = permissionsService.getPermissions().pipe(
+      map(p => p.canDeleteQuestion)
+    );
+  }
 }
